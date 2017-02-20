317be3dbe2811a5bcbd20c6b31e112619b8377fc
joanca <jcaravenae@gmail.com>
2017-02-20 13:07:07 -0300

    Third assignment

---
+++ src/Triangle03.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Triangle03 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        int n = scan.nextInt();
+
+        for(int i = 0; i < n; i++) {
+            System.out.println("*");
+        }
+    }
+}
