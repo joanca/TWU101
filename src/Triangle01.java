531e150c9f195d842dde7e46585b224a93ac1f91
joanca <jcaravenae@gmail.com>
2017-02-20 12:54:17 -0300

    First assignment

--- src/Triangle01.java
+++ src/Triangle01.java
@@ -1,8 +1,5 @@
-/**
- * Created by juancarlosaravena on 20-02-17.
- */
 public class Triangle01 {
     public static void main(String[] args) {
-        System.out.println("asd");
+        System.out.println("*");
     }
 }
